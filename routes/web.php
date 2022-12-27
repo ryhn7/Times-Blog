236009293b33ec0ceba046c35b4bf46f9276f10d
Rayhan Illyas <rayhan205732@gmail.com>
2022-12-27 15:53:16 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Rayhan Gallery',
+        'email' => 'gallery@Gmail.com',
+        'image' => 'https://source.unsplash.com/500x500'
+    ]);
+});
+
+Route::get('/post', function () {
+    return view('posts');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./resources/**/*.{blade.php,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
