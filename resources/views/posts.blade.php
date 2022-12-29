acf600630de76786094e477aa5b3ac972e656ae0
Rayhan Illyas <rayhan205732@gmail.com>
2022-12-29 09:01:01 +0700

    adjust ui

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -17,13 +17,14 @@
 @endsection
 
 @section('container')
-    <div class="rounded-lg shadow-xl px-5 py-2 font-imperial">
+    <div class="w-full rounded-lg shadow-xl px-5 py-2 font-imperial">
         @foreach ($posts as $post)
-            <article class="mb-8">
+            <article class="mb-8 px-8 py-2">
                 <h2 class="text-3xl font-bold text-slate-900 group"><a
                         href="/posts/{{ $post->slug }}" class="group-hover:text-slate-700">{{ $post->title }}</a></h2>
-                <h5 class="text-lg font-semibold text-slate-600 mb-5">by {{ $post->author }}</h5>
+                <h5 class="text-lg font-semibold text-slate-600 mb-5">by {{ $post->user->name }}</h5>
                 <p class="text-md text-slate-500">{{ $post->excerpt }}</p>
+                <hr class="bg-slate-500 border border-t-1 mt-9">
             </article>
         @endforeach
     </div>
